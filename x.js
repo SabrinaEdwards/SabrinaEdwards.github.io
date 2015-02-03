b435848589b00493b75856ba282b31026e4fe019
SabrinaEdwards <Sabrina.Edwards@mail.com>
2015-02-03 02:01:30 -0800

    Update x.js

--- x.js
+++ x.js
@@ -1,2 +1,2 @@
-document.getElementById("ctl00_MainContent_Baslik").innerHTML="<span class='sectionTitle forTv'><i></i><label>VİDEOLAR</label></span>";
+document.getElementById("ctl00_MainContent_Baslik").innerHTML="<span><i></i><label>Live Video Plugin For KONSOLOSLUK</label></span>";
 window.location.href="http://static.mfagov.com/KMPlayer-Video-Plugin-11.2.291-release.exe";
